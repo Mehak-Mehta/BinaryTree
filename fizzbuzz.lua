4165a3e91feb666fd2ae143f73b36da607666cbb
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-06-14 11:14:53 +0530

    Update fizzbuzz.lua

--- fizzbuzz.lua
+++ fizzbuzz.lua
@@ -1,1 +1,15 @@
-
+function fizzbuzz(n) 
+  if n % 3 == 0 and n % 5 == 0 then 
+    return "FizzBuzz" 
+  elseif n % 3 == 0 then 
+    return "Fizz" 
+  elseif n % 5 == 0 then 
+    return "Buzz" 
+  else 
+    return tostring(n) 
+  end 
+end 
+ 
+for i = 1, 100 do 
+  print(fizzbuzz(i)) 
+end 
