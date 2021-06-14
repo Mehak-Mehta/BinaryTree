4a1aa8ec0ebdb34c24eadb82e529ac282fef25ed
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-06-14 11:18:10 +0530

    Update fizzbuzz.cr

--- fizzbuzz.cr
+++ fizzbuzz.cr
@@ -1,1 +1,31 @@
+module Fizzbuzz
+  VERSION = "0.1.0"
 
+  def div_by_three(n : Int32)
+    n % 3 == 0
+  end
+
+  def div_by_five(n : Int32)
+    n % 5 == 0
+  end
+
+  def div_by_fifteen(n : Int32)
+    n % 15 == 0
+  end
+end
+
+include Fizzbuzz
+
+(1..100).each do |num|
+  answer = if div_by_fifteen num
+    "FizzBuzz"
+  elsif div_by_three num
+    "Fizz"
+  elsif div_by_five num
+    "Buzz"
+  else
+    num
+  end
+
+  puts answer
+end
