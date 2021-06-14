71caab8d871baa4f24e8ac4799cbc6e40a6295c2
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-06-14 11:10:31 +0530

    Update fizzbuzz.hs

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,1 +1,9 @@
-
+fizzbuzz :: [Integer] -> [String]
+fizzbuzz [] = []
+fizzbuzz xs = [
+	let fz = if mod x 3 == 0 then "fizz" else [] in
+	let bz = if mod x 5 == 0 then "buzz" else [] in
+	let fzbz = fz ++ bz in
+	if null fzbz then show x else fzbz |
+		x <- xs
+	]
