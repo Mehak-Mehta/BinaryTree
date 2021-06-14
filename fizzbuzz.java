9c05bcb37a792b7b52e47246698d9f3492ebf3cd
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-06-14 11:13:54 +0530

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,1 +1,15 @@
-
+public class main {
+	public static void main(String[] arg) {
+		
+		for(int i = 1; i < 100; i++) {
+			if(i % 5 == 0 && i % 7 == 0)
+				System.out.println("FizzBuzz");
+			else if(i % 5 == 0)
+				System.out.println("Fizz");
+			else if(i % 7 == 0)
+				System.out.println("Buzz");
+			else
+				System.out.println(i);
+		}
+	}
+}
