dd80a5e8de7736be93935a28f45cf40a20490984
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-06-14 15:12:16 +0530

    Update fizzbuzz.pl

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -1,1 +1,16 @@
+#!/usr/bin/perl
 
+
+use strict;
+use warnings;
+use diagnostics;
+
+use 5.10.0;
+
+for my $n (1..100) {
+    # Print word if...
+    !($n % 15) ?    say "FizzBuzz"    :   
+    !($n % 3)  ?    say "Fizz"        :   
+    !($n % 5)  ?    say "Buzz"        :   
+                    say "$n";            
+}
