7bd926f83cd4d52b9ad99e35d5eff611b7de818a
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-06-14 11:08:29 +0530

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int fbindex(int n) {
+    int i = 0;
+    if (n % 3 == 0) i += 1;
+    if (n % 5 == 0) i += 2;
+    return i;
+}
+
+int main(void) {                                 
+    const char *fb[] = {"%d\n", "Fizz\n", "Buzz\n", "FizzBuzz\n"};
+    for (int i = 1; i <= 100; i++) printf(fb[fbindex(i)], i);                
+}
