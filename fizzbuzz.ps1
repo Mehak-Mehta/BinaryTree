82c6f669bf7e76dba23960c0477cf63d2e63010f
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-06-14 11:06:01 +0530

    Update fizzbuzz.ps1

--- fizzbuzz.ps1
+++ fizzbuzz.ps1
@@ -1,1 +1,14 @@
+for($x = 1; $x -le 100; $x++) {
+  $Threes = $x % 3
+  $Fives = $x % 5
 
+  if (($Threes -eq 0) -and ($Fives -eq 0)) {
+    Write-Output "FizzBuzz"
+  } else if ($Threes -eq 0) {
+    Write-Output "Fizz"
+  } else if ($Fives -eq 0) {
+    Write-Output "Buzz"
+  } else {
+    Write-Output $x
+  }
+}
