43abd5b7ebb25b42da86783d000023daea669890
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-06-14 11:07:08 +0530

    Update fizzbuzz.purs

--- fizzbuzz.purs
+++ fizzbuzz.purs
@@ -1,1 +1,25 @@
+module Main where
 
+import Prelude (bind, mod, pure, (<$>), (<<<), (==))
+import Data.List (List, (..))
+import Data.Either (Either(..))
+
+isDivisibleBy :: Int -> String -> Either String Int -> Either String Int
+isDivisibleBy divider replacer value = do
+  v <- value
+  if mod v divider == 0
+    then Left replacer
+    else Right v
+
+
+fizz :: Either String Int -> Either String Int
+fizz = isDivisibleBy 5 "Fizz"
+
+buzz :: Either String Int -> Either String Int
+buzz = isDivisibleBy 3 "Buzz"
+
+fizzBuzz :: Either String Int -> Either String Int
+fizzBuzz = fizz <<< buzz <<< (isDivisibleBy 15 "FizzBuzz")
+
+result :: List (Either String Int)
+result = fizzBuzz <<< pure <$> (1 .. 100)
